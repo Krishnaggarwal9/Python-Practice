f3427df1a1ad9407dede8c81d4fe338f46a035e7
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-01-21 00:23:09 +0530

    adding swapping of two numbers

---
+++ Basic/swapping_numb.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+a,b = b,a
+print("Your given first number after swapping: ",a)
+print("Your given second number after swapping: ",b)
