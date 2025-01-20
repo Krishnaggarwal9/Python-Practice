7128a7019523f21494aeb15a55ae2eabd537babd
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-01-21 01:02:09 +0530

    adding To check whether the number is Odd or Even

---
+++ Basic/odd_even.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your number: "))
+if  a%2==1:
+    print("Your given number is Odd")
+elif a==0 :
+    print("Your given number is zero, But we can consider it as even number")
+else:
+    print("Your given number is Even")
