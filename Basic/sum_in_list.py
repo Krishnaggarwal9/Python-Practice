4be57595e5d99d36360d4d2cc3063570f7ca9203
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-01-23 23:50:50 +0530

    adding sum of numbs in a list

---
+++ Basic/sum_in_list.py
@@ -0,0 +1,7 @@
+a= int(input("Enter the first numbers to get the sum of them: "))
+b= int(input("Enter the second numbers to get the sum of them: "))
+c= int(input("Enter the third numbers to get the sum of them: "))
+d= int(input("Enter the fourth numbers to get the sum of them: "))
+
+e=[a , b , c , d]
+print("The sum of numbers in the list is: ",sum(e))
