ae6c2640322168e05cdfea33d3fd8512c0b9c4eb
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-02-05 00:27:18 +0530

    adding iterating a list

---
+++ Basic/iterate_list.py
@@ -0,0 +1,7 @@
+lis = int(input("Enter the number of string you want: "))
+lis1=[]
+for _ in range(lis):
+    lis1.append(input("Enter the string: "))
+    print("Your list is: " +str(lis1))
+for i in lis1:
+    print("Your iterated list: ",i)
