46f4ad9f4fbc0fc5d28aeb2c21ee6bb2e6b3bb70
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-02-05 01:28:19 +0530

    adding Multiplication table

---
+++ Basic/multiplication_table.py
@@ -0,0 +1,9 @@
+user_mult = int(input("Enter your number: "))
+multiplier = user_mult
+counter = 1
+
+while counter <=10:
+
+    answer = multiplier * counter
+    print(multiplier, "X", counter, "=", answer)
+    counter +=1
