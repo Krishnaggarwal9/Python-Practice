3517ef18cc5b1f0370cd150ed21a2f64b4a95c82
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-05-13 21:42:48 +0530

    adding the perimeter of a rectangle

---
+++ Basic/perimeter_of_rectangle.py
@@ -0,0 +1,4 @@
+Length = int(input("Enter the Length of the rectangle: "))
+Width = int(input("Enter the Width of the rectangle: "))
+Perimeter = 2 *(Length + Width)
+print("The perimeter of the rectangle is ",Perimeter)
