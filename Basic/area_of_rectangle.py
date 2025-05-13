ddac5391ee3be0466781d65492f2bff3a670e239
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-05-13 21:40:32 +0530

    adding the area of rectangle

---
+++ Basic/area_of_rectangle.py
@@ -0,0 +1,4 @@
+Length = int(input("Enter the Length of the Rectangle in cm: "))
+Width = int(input("Enter the Width of the Rectangle in cm: "))
+Area = Length * Width
+print(f"The area of rectangle with Length {Length} and Width {Width} is : ",Area ) 
