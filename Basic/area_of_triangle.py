ef1736f5cc13109ecc9868f85b2efe3c093d88f6
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-05-13 21:37:55 +0530

    adding area of triangle

---
+++ Basic/area_of_triangle.py
@@ -0,0 +1,4 @@
+Breadth = int(input("Enter the Length of the Rectangle in cm: "))
+Height = int(input("Enter the Width of the Rectangle in cm: "))
+Area = 1/2*Breadth * Height
+print(f"The area of rectangle with Length {Breadth} and Width {Height} is : ",Area ) 
