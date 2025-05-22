aad0689bdad1ddf6f815ea69fd4731070e394a6b
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-05-22 20:51:57 +0530

    Now we can store letters also ,earlier we can store only numbers

--- Basic/remove_dublicate.py
+++ Basic/remove_dublicate.py
@@ -1,9 +1,9 @@
 num_len= int(input("Enter the number of elements in list: "))
 input_list = []
 for _ in range(num_len):
-     input_list.append(int(input("Enter the numbers:  ")))
-res =[]     
-for num in input_list:
-    if num not in res:
-        res.append(num)
-print("Your removed duplicate list is: ",res)
+     input_list.append(input("Enter the numbers:  "))
+removed_item =[]     
+for items in input_list:
+    if items not in removed_item:
+        removed_item.append(items)
+print("Your removed duplicate list is: ",removed_item)
