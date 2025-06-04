f85ac59480c69fd4c5fae455e24065a3e7606519
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-06-04 12:46:50 +0530

    adding removing leading and tailing spaces

---
+++ Basic/removing_leading_and_tailing_spaces.py
@@ -0,0 +1,5 @@
+string1 = input("Enter your string if it contains leading and tailing spaces:- ")
+
+Removing_spaces = string1.strip()
+
+print("Your string withot leading and tailing spaces are:- ",Removing_spaces)
