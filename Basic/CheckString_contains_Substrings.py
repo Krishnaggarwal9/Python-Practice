47ee4e1796b74d4f74b4cd104e44e3bec5112b58
Krishnaggarwal9 <krihnapd883@gmail.com>
2025-06-04 12:30:56 +0530

    addding if the string contains substring

---
+++ Basic/CheckString_contains_Substrings.py
@@ -0,0 +1,9 @@
+String1 = input("Enter the your String:- ")
+
+Substring = input("Enter the substring:- ")
+
+if "Substring" in String1:
+    print("Yes it is present!")
+
+else:
+    print("No, It is not present")
